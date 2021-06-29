3ccdca5baad9bf121420ae8763d11da0dec84734
Arkadiraf <arkadiraf@gmail.com>
2021-06-29 14:25:07 +0300

    updates

--- Matlab/Development_Matlab_2018/dcMotorControlInit.m
+++ Matlab/Development_Matlab_2018/dcMotorControlInit.m
@@ -1,7 +1,8 @@
 % Start up script for the DC Motor Control Simulation: dcMotorControlSim.slx
 % declare variables for the simulation
 sampleTime = 0.01;  % Simulation Sample time in seconds.
-gearRatio = 100;    % Simulation Gear Ratio
-mass = 0;           % gram default 100g
+desiredCMD = 300;   % desired Step size, 100 for 1:100 gear, 300 for 1:30 gear
+gearRatio =  29.86; % Simulation Gear Ratio
+mass = 0;           % gram - default 100g
 radius = 2.5;       % cm
 inertiaDisc = 1/2*mass*radius^2 % g*cm^2 
