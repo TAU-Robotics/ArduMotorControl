fccc1965a5809e0bb2f9e68e9b6d636c8a0fcca8
Arkadiraf <arkadiraf@gmail.com>
2022-12-11 15:25:35 +0200

    Create TAU_Ard_Encoder_Base.ino

---
+++ TAU-ARD_V01/Arduino/TAU_Ard_Encoder_Base/TAU_Ard_Encoder_Base.ino
@@ -0,0 +1,57 @@
+//Encoder Example
+
+// Define Pins
+#define ENCODER_PINA 2
+#define ENCODER_PINB 3
+
+// encoder variables
+volatile int encoderCounts = 0;
+
+// Encoder ISR functions - Interupt Service Routine
+void encoderA();
+void encoderB();
+
+
+void setup() {
+  // initialize serial communication at 115200 bits per second:
+  Serial.begin (115200);
+  
+  // initialize encoder, attache ISR functions
+  pinMode(ENCODER_PINA, INPUT);
+  pinMode(ENCODER_PINB, INPUT);
+   // Attached interrupt to encoder pins
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PINA), encoderA, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PINB), encoderB, CHANGE);
+
+  Serial.print("Encoder_Value");
+}
+
+void loop() {
+  // print encoder position
+  Serial.println(encoderCounts);
+  delay(100);
+}
+
+// EncoderA ISR
+void encoderA() {
+  // look for a low-to-high on channel B
+  if (digitalRead(ENCODER_PINA) == HIGH) {
+    // check channel A to see which way encoder is turning
+    digitalRead(ENCODER_PINB) ? encoderCounts++ : encoderCounts--;    
+  }else{
+    // check channel A to see which way encoder is turning
+    digitalRead(ENCODER_PINB) ? encoderCounts-- : encoderCounts++; 
+  } 
+} // End EncoderA ISR
+
+// EncoderB ISR
+void encoderB() {
+  // look for a low-to-high on channel B
+  if (digitalRead(ENCODER_PINB) == HIGH) {
+    // check channel A to see which way encoder is turning
+    digitalRead(ENCODER_PINA) ? encoderCounts-- : encoderCounts++;    
+  }else{
+    // check channel A to see which way encoder is turning
+    digitalRead(ENCODER_PINA) ? encoderCounts++ : encoderCounts--; 
+  }
+} // End EncoderB ISR
